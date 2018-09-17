0504177ff87fa669fc8e7497e3352ca50c5dd94c
Ben Robinson <mentionbenjamin@gmail.com>
2018-09-17 16:59:30 +0100

    gets data from the store and dispatches actions

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import uuidv4 from 'uuid/v4';
+import { ADD_BOOKMARK, DELETE_BOOKMARK } from './types';
+
+export const addBookmark = ({ title, url }) => ({
+  type: ADD_BOOKMARK,
+  payload: {
+    id: 123456,
+    title,
+    url
+  }
+});
+
+export const deleteBookmark = id => ({
+  type: DELETE_BOOKMARK,
+  payload: {
+    id
+  }
+});
